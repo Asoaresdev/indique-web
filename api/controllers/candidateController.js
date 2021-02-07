5dde53fc573ed0f1b9289efd65926b2256647e6e
Deisemaya <somayagomes@gmail.com>
2021-02-07 17:33:12 -0300

    feat: configurando o bcryp para criptografar as senha de cadastro e validar a senha de login

--- api/controllers/candidateController.js
+++ api/controllers/candidateController.js
@@ -3,6 +3,9 @@ const fs= require('fs')
 const path= require('path')
 const bcrypt  = require('bcrypt')
 
+let candidateJson = path.join('candidate.json')
+let Hash = bcrypt.hashSync("1234", 10)
+
 const candidateController = {
 
     index: (req,res)=> {
@@ -20,8 +23,8 @@ const candidateController = {
     },
     Store: (req, res)=>{
         let {name, email, phone, about, escolaridade,
-             instituicao, curso, area} = req.body;
-        fs.readFileSync();
+             instituicao, curso, area, senha} = req.body;
+       
 
         //criando logica do ID
         let id = candidade.lenght + 1
@@ -35,22 +38,32 @@ const candidateController = {
             escolaridade: escolaridade,
             insittuicao:instituicao,
             curso: curso,
-            area:area
+            area:area,
+            senha:senhaC
 
 
         });
         candidate = JSON.stringify(candidate)
+        let senhaC = bcrypt.hashSync(senha,10)
 
         fs.writeFileSync(path.join('database','candidate.json'),candidate)
 
-
-            // res.redirect('/')
-        
+     res.send(" usuario cadastrado com sucesso")
+           
     },
     login: (
         req, res) =>{
         const { email, senha} = req.bady
-    }
+        let candidateSalvo = fs.readFileSync(candidateJson, {encoding:"utf -8"});
+        candidateSalvo = JSON.parse(candidateSalvo)
+        if(email!= candidateSalvo.email){
+            return res.send( ' usuario Invalido')
+        }
+        if(!bcrypt.compareSync(senha , candidateSalvo.senha)){
+            return res.send("senha invalida")
+        }
+
+    }   
 }
 
 module.exports = candidateController
